d33ad897e859d7616c00faeb91e2c69d19da23d6
徐小璇 <xuxiaoxuan@lovol.com>
2024-07-17 14:54:51 +0800

    fix bug

--- test_conformance.py
+++ test_conformance.py
@@ -1,7 +1,31 @@
 import pm4py
 import sys
 if __name__ == "__main__":
-    BPIC13_i = pm4py.read_xes('BPIC13_i.xes')
+    BPIC12_output_file = '/app/data/BPIC12.xes'
+    BPIC13_cp_output_file = '/app/data/BPIC13_cp.xes'
+    BPIC13_i_output_file = '/app/data/BPIC13_i.xes'
+    BPIC14_f_output_file = '/app/data/BPIC14_f.xes'
+    BPIC15_1f_output_file = '/app/data/BPIC15_1f.xes'
+    BPIC15_2f_output_file = '/app/data/BPIC15_2f.xes'
+    BPIC15_3f_output_file = '/app/data/BPIC15_3f.xes'
+    BPIC15_4f_output_file = '/app/data/BPIC15_4f.xes'
+    BPIC15_5f_output_file = '/app/data/BPIC15_5f.xes'
+    BPIC17_f_output_file = '/app/data/BPIC17_f.xes'
+    RTFMP_output_file = '/app/data/RTFMP.xes'
+    SEPSIS_output_file = '/app/data/SEPSIS.xes'
+    # 加载解压后的 .xes 文件
+    BPIC12 = pm4py.read_xes(BPIC12_output_file)
+    BPIC13_cp = pm4py.read_xes(BPIC13_cp_output_file)
+    BPIC13_i = pm4py.read_xes(BPIC13_i_output_file)
+    BPIC14_f = pm4py.read_xes(BPIC14_f_output_file)
+    BPIC15_1f = pm4py.read_xes(BPIC15_1f_output_file)
+    BPIC15_2f = pm4py.read_xes(BPIC15_2f_output_file)
+    BPIC15_3f = pm4py.read_xes(BPIC15_3f_output_file)
+    BPIC15_4f = pm4py.read_xes(BPIC15_4f_output_file)
+    BPIC15_5f = pm4py.read_xes(BPIC15_5f_output_file)
+    BPIC17_f = pm4py.read_xes(BPIC17_f_output_file)
+    RTFMP = pm4py.read_xes(RTFMP_output_file)
+    SEPSIS = pm4py.read_xes(SEPSIS_output_file)
     if len(sys.argv) > 1:
         # 获取第一个命令行参数
         param = sys.argv[1]
